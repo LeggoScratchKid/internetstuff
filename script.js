0cd486a66031fc67333e213d85a77a8289ed02ee
LeggoScratchKid <isaac.lsk.2025@gmail.com>
2026-05-29 12:58:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 let buttonClicks = 0;
 let button = document.getElementById("clicky");
 
-clicky.onclick = function () {
+var mouseDown = new Audio('sounds/click_down.wav');
+var mouseUp = new Audio('sounds/click_up.wav');
+
+function clickbutton () {
   buttonClicks++;
   button.innerHTML = "Button CLicks: " + buttonClicks;
+  mouseDown.play();
+}
+
+function declickbutton {  
+  mouseUp.play();
 }
