cf88764ab7830cdeff09b9610f8067b57b2937db
LeggoScratchKid <isaac.lsk.2025@gmail.com>
2026-06-02 13:03:00 -0600

    Create danger.js

---
+++ danger.js
@@ -0,0 +1,24 @@
+let textbox = document.getElementByID("punishtext");
+let page = document.getElementByID("body");
+
+function danger() {
+  let punishment = math.floor(math.random()*2)+1;
+  if (punishment == 1) {
+    spare()
+  };
+  if (punisment == 2) {
+    deleteWebpage()
+  };
+  if (punishment == 3) {
+    insult()
+  };
+};
+
+function spare() {
+  textbox.style.color = "red";
+  textbox.text = "The button has spared you. Take this chance to flee.";
+};
+
+function deleteWebpage() {
+  page.text = "";
+};
