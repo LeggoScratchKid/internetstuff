fc39a757dac453b707b3cc2e7050b3d23c247a14
LeggoScratchKid <isaac.lsk.2025@gmail.com>
2026-06-03 13:04:56 -0600

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,29 @@ function clickbutton() {
 function declickbutton() {  
   mouseUp.play();
 }
+
+let textbox = document.getElementByID("punishtext");
+let page = document.getElementByID("body");
+
+function danger() {
+  let punishment = Math.floor(Math.random()*2)+1;
+  if (punishment == 1) {
+    spare()
+  };
+  if (punisment == 2) {
+    deleteWebpage()
+  };
+  if (punishment == 3) {
+    insult()
+  };
+  spare()
+};
+
+function spare() {
+  textbox.style.color = "red";
+  textbox.innerHTML = "The button has spared you. Take this chance to flee.";
+};
+
+function deleteWebpage() {
+  page.innerHTML = "";
+};
